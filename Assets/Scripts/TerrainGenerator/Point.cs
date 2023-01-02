e764da85573f92ef49c4850c51b65e83135b0cab
Kuro <guitaristkun@gmail.com>
2023-01-02 03:07:46 +0300

    attached built-in simplex3d for testing

--- Assets/Scripts/TerrainGenerator/Point.cs
+++ Assets/Scripts/TerrainGenerator/Point.cs
@@ -2,26 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Point : MonoBehaviour {
-    public Vector3Int coordinates;
+namespace TerrainGenerator {
+    public class Point : MonoBehaviour {
+        public Vector3Int coordinates;
 
-    public static Point CreatePoint(Vector3Int coordinates, bool isWhite = true) {
-        GameObject prefab;
-        GameObject pointGameObject;
-        Point point;
-        string resourcePath;
+        public static Point CreatePoint(Vector3Int coordinates) {
+            GameObject prefab;
+            GameObject pointGameObject;
+            Point point;
+            string resourcePath;
 
-        if (isWhite) {
-            resourcePath = "Prefabs/Point (white)";
-        } else {
             resourcePath = "Prefabs/Point (black)";
+            prefab = Resources.Load<GameObject>(resourcePath);
+            pointGameObject = Instantiate(prefab, coordinates, Quaternion.identity);
+            point = pointGameObject.AddComponent<Point>();
+            point.coordinates = coordinates;
+            point.gameObject.name = $"Point ({coordinates.x} {coordinates.y} {coordinates.z})";
+            return point;
         }
-
-        prefab = Resources.Load<GameObject>(resourcePath);
-        pointGameObject = Instantiate(prefab, coordinates, Quaternion.identity);
-        point = pointGameObject.AddComponent<Point>();
-        point.coordinates = coordinates;
-        point.gameObject.name = $"Point ({coordinates.x} {coordinates.y} {coordinates.z})";
-        return point;
     }
 }
--- Assets/Scripts/TerrainGenerator/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator/TerrainGenerator.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 namespace TerrainGenerator { 
 
     [ExecuteInEditMode]
     public class TerrainGenerator : MonoBehaviour {
-        public double level = 0.5;
+        [Range(0.0f, 1.0f)]
+        public float level = 0.5f;
+        [Range(0.05f, 0.2f)]
+        public float scale = 0.1f;
         public Vector3 size = new Vector3(16, 16, 16);
 
         bool settingsUpdated;
         Queue<Point> points = new Queue<Point>();
+        GameObject pointHolder;
+        const string pointHolderName = "Point Holder";
 
         void Awake() {
             print("Awake");
@@ -35,6 +41,7 @@ namespace TerrainGenerator {
             }
             Point[] oldPoints = FindObjectsOfType<Point>();
             for (int i = 0; i < oldPoints.Length; ++i) {
+                oldPoints[i].gameObject.SetActive(true);
                 DestroyImmediate(oldPoints[i].gameObject, false);
             }
         }
@@ -42,21 +49,35 @@ namespace TerrainGenerator {
         void InstantiatePoint(int x, int y, int z) {
             Point point;
             Vector3Int coordinates;
-            bool isWhite;
+            float sampleNoise;
 
-            isWhite = Random.Range(0f, 1f) > 0.5f;
             coordinates = new Vector3Int(x, y, z);
-            point = Point.CreatePoint(coordinates, isWhite);
-            point.transform.parent = transform;
-            points.Enqueue(point);
+            sampleNoise = noise.snoise(new float3(x, y, z) * scale);
+            if (sampleNoise > level) {
+                point = Point.CreatePoint(coordinates);
+                point.transform.parent = pointHolder.transform;
+                point.transform.localScale *= sampleNoise * math.sqrt(2);
+                points.Enqueue(point);
+            }
         }
 
         void GenerateMesh() {
+            CreatePointHolder();
             for (int x = 0; x < size.x; ++x) {
                 for (int y = 0; y < size.y; ++y) {
                     for (int z = 0; z < size.z; ++z) {
                         InstantiatePoint(x, y, z);
                     }
+                }
+            }
+        }
+
+        void CreatePointHolder() {
+            if (pointHolder == null) {
+                if (GameObject.Find(pointHolderName)) {
+                    pointHolder = GameObject.Find(pointHolderName);
+                } else {
+                    pointHolder = new GameObject(pointHolderName);
                 }
             }
         }
