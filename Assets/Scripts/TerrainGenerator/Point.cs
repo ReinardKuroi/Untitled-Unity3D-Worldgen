30a26c6e8a927ca151c8546f8bc56ee5acb277fb
Kuro <guitaristkun@gmail.com>
2023-01-02 21:55:49 +0300

    modified point to separate spatial hashing into a method

--- Assets/Scripts/TerrainGenerator/Point.cs
+++ Assets/Scripts/TerrainGenerator/Point.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 namespace TerrainGenerator {
     public class Point {
         public Vector3Int Coordinates { get; private set; }
-        public float Level { get; private set; }
+        public float Density { get; private set; }
         public GameObject GameObject { get; set; }
-        public int Hash { get { return (Coordinates.x * 73856093 ^ Coordinates.y * 19349663 ^ Coordinates.z * 83492791) % System.Int32.MaxValue; } }
+        public int Hash { get; private set; }
         private readonly string prefabName = "Prefabs/Point (black)";
 
-        public Point(Vector3Int coordinates, float level) {
+        public Point(Vector3Int coordinates, float density) {
             this.Coordinates = coordinates;
-            this.Level = level;
+            this.Density = density;
+            this.Hash = CalculateHash(coordinates.x, coordinates.y, coordinates.z);
         }
 
         public GameObject LoadPrefab() {
             return Resources.Load<GameObject>(prefabName);
+        }
+
+        public static int CalculateHash(int x, int y, int z) {
+            return (x * 73856093 ^ y * 19349663 ^ z * 83492791) % System.Int32.MaxValue;
         }
     }
 }
