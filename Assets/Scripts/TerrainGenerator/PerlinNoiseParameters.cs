ee5ba0c8043cff69be1943d8f1af132c873fb485
Kuro <guitaristkun@gmail.com>
2023-01-08 17:17:51 +0300

    extract perlin noise parameters

---
+++ Assets/Scripts/TerrainGenerator/PerlinNoiseParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace TerrainGenerator {
+    [System.Serializable]
+    public struct PerlinNoiseParameters {
+        [Range(0, 1000)]
+        public int frequency;
+        [Range(0, 10)]
+        public float persistence;
+        [Range(0, 10)]
+        public float lacunarity;
+        [Range(1, 10)]
+        public int octaves;
+    }
+}
