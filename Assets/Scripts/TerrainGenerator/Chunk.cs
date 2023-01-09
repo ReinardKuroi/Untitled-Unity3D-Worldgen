d2064015174c14cc9614e237566c5240e45faf3b
Kuro <guitaristkun@gmail.com>
2023-01-09 22:56:02 +0300

    chunk more autonomous now

--- Assets/Scripts/TerrainGenerator/Chunk.cs
+++ Assets/Scripts/TerrainGenerator/Chunk.cs
@@ -11,6 +11,7 @@ namespace TerrainGenerator {
         public int Size { get; set; }
         [SerializeField]
         public Vector3 Center { get { return Size * (Coordinates + Vector3.one * 0.5f); } }
+
         public bool Empty { get { return mesh.vertexCount == 0 || mesh.triangles.Length == 0; } }
 
         public IMeshGenerator chunkGenerator;
@@ -18,11 +19,37 @@ namespace TerrainGenerator {
 
         [HideInInspector]
         public Mesh mesh;
+        Mesh collisionMesh;
         MeshCollider meshCollider;
         MeshFilter meshFilter;
         MeshRenderer meshRenderer;
+        bool generateCollider;
 
-        public void Setup(Material material, bool generateCollider) {
+        public static Chunk Create() {
+            Chunk chunk;
+            GameObject chunkObject = new();
+            chunk = chunkObject.GetComponent<Chunk>();
+            if (chunk == null) {
+                chunk = chunkObject.AddComponent<Chunk>();
+            }
+            return chunk;
+        }
+
+        public void Destroy() {
+            DestroyImmediate(gameObject, false);
+        }
+
+        public void Init(Vector3Int coordinates, int chunkSize, Transform chunkRoot) {
+            Coordinates = coordinates;
+            Size = chunkSize;
+            name = $"Chunk ({Coordinates})";
+            transform.parent = chunkRoot;
+            transform.localPosition = Coordinates * Size;
+            transform.localRotation = Quaternion.identity;
+        }
+
+        public void SetupMesh(Material material, bool generateCollider) {
+            this.generateCollider = generateCollider;
             meshFilter = GetComponent<MeshFilter>();
             meshRenderer = GetComponent<MeshRenderer>();
             meshCollider = GetComponent<MeshCollider>();
@@ -51,15 +78,19 @@ namespace TerrainGenerator {
                 meshFilter.sharedMesh = mesh;
             }
             
-
             if (generateCollider) {
                 if (meshCollider.sharedMesh == null) {
+                    meshCollider.cookingOptions = MeshColliderCookingOptions.None;
+                    meshCollider.cookingOptions = MeshColliderCookingOptions.UseFastMidphase;
                     meshCollider.sharedMesh = mesh;
                 }
-                gameObject.isStatic = false;
-                meshCollider.enabled = false;
-                meshCollider.enabled = true;
+                ForceUpdateCollider();
             }
+        }
+
+        private void ForceUpdateCollider() {
+            meshCollider.enabled = false;
+            meshCollider.enabled = generateCollider;
         }
 
         public void Enable() {
@@ -74,12 +105,17 @@ namespace TerrainGenerator {
             }
         }
 
-        internal void SetMesh() {
+        public void SetMesh() {
             chunkGenerator.SetMesh(mesh);
+            if (!Empty && generateCollider) {
+                ForceUpdateCollider();
+            }
         }
 
-        internal void Destroy() {
-            DestroyImmediate(gameObject, false);
+        public void ResetMesh() {
+            if (mesh != null) {
+                mesh.Clear();
+            }
         }
     }
 }
